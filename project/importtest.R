9f51b3acaddb5558c32c926764ee86c5f9bfcb40
shaw-a-d <ashaw93@gatech.edu>
2025-07-25 18:57:10 -0400

    update internal link

--- project/importtest.R
+++ project/importtest.R
@@ -7,28 +7,28 @@ library(janitor)
 library(dplyr)
 library(ggplot2)
 
-setwd('~/Documents/GitHub/6011_coding/6011_project')
+setwd("~/Documents/GitHub/6011_coding/6011_project")
 
-file_list <- list.files('project_data/generator', full.names = TRUE)
+file_list <- list.files("project_data/generator", full.names = TRUE)
 
 solar_state_year <- data.frame()
 
 for (file in file_list) {
   year <- as.numeric(gsub("\\D", "", basename(file)))
-  
+
   df <- read_xlsx(file, sheet = 1) |>
     clean_names()
-  
+
   df_solar <- df |>
-    filter(technology == 'Solar Photovoltaic') |>
+    filter(technology == "Solar Photovoltaic") |>
     group_by(state) |>
     reframe(total_mw = sum(nameplate_capacity_mw)) |>
     mutate(year = year)
-  
+
   solar_state_year <- bind_rows(solar_state_year, df_solar)
-  }
+}
 
 View(solar_state_year)
 
-ggplot(solar_state_year, aes(x= year, y=total_mw, color = state)) +
+ggplot(solar_state_year, aes(x = year, y = total_mw, color = state)) +
   geom_line()
